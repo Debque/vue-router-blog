afa586a6a6e4838de9173f4893fa7c44fcc5b574
Debque <90905399+Debque@users.noreply.github.com>
2026-05-24 21:56:33 +0100

    chore: remove/update purple favicon from browser tab

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -38,7 +38,7 @@ function toggleDarkMode() {
             <path d="M2 17L12 22L22 17" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M2 12L12 17L22 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
-          <span class="logo-text">Vue Router Blog</span>
+          <span class="logo-text">STELLA'S BLOG</span>
         </router-link>
       </div>
 
